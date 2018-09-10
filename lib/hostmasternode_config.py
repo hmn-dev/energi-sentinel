21f676da104a0450de9352ebc08ea932756a9530
hmn-dev <40977238+hmn-dev@users.noreply.github.com>
2018-09-10 17:22:07 +0530

    Update and rename vivo_config.py to hostmasternode_config.py

--- lib/hostmasternode_config.py
+++ lib/hostmasternode_config.py
@@ -7,11 +7,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 from misc import printdbg
 
 
-class VivoConfig():
+class hostmasternodeConfig():
 
     @classmethod
     def slurp_config_file(self, filename):
-        # read vivo.conf config but skip commented lines
+        # read hostmasternode.conf config but skip commented lines
         f = io.open(filename)
         lines = []
         for line in f:
@@ -20,27 +20,27 @@ class VivoConfig():
             lines.append(line)
         f.close()
 
-        # data is vivo.conf without commented lines
+        # data is hostmasternode.conf without commented lines
         data = ''.join(lines)
 
         return data
 
     @classmethod
     def get_rpc_creds(self, data, network='mainnet'):
-        # get rpc info from vivo.conf
+        # get rpc info from hostmasternode.conf
         match = re.findall(r'rpc(user|password|port)=(.*?)$', data, re.MULTILINE)
 
         # python >= 2.7
         creds = {key: value for (key, value) in match}
 
-        # standard Vivo defaults...
+        # standard hostmasternode defaults...
         default_port = 9998 if (network == 'mainnet') else 19998
 
-        # use default port for network if not specified in vivo.conf
+        # use default port for network if not specified in hostmasternode.conf
         if not ('port' in creds):
             creds[u'port'] = default_port
 
-        # convert to an int if taken from vivo.conf
+        # convert to an int if taken from hostmasternode.conf
         creds[u'port'] = int(creds[u'port'])
 
         # return a dictionary with RPC credential key, value pairs
