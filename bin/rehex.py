872514c603e05820299ca3f571c39386cc42fcaf
hmn-dev <40977238+hmn-dev@users.noreply.github.com>
2019-01-01 19:23:18 +0530

    Update rehex.py

--- bin/rehex.py
+++ bin/rehex.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import sys
 import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
-import hostmasternodelib
+import energilib
 # ============================================================================
 usage = "%s <hex>" % sys.argv[0]
 
@@ -15,7 +15,7 @@ if len(sys.argv) < 2:
     print(usage)
     sys.exit(1)
 else:
-    obj = hostmasternodelib.deserialise(sys.argv[1])
+    obj = energilib.deserialise(sys.argv[1])
 
 pdb.set_trace()
 1
